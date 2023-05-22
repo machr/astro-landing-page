a7e1076936005d42ac7b76b720d9070af79a0b12
Mark Christiansen <hi@powellmark.com>
2023-05-22 15:32:23 +1000

    Add real "Last played" data

--- src/lib/spotify.js
+++ src/lib/spotify.js
@@ -3,7 +3,7 @@ const client_secret = import.meta.env.SPOTIFY_CLIENT_SECRET
 const refresh_token = import.meta.env.SPOTIFY_REFRESH_TOKEN
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 const encoded = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
-const scope = 'user-read-recently-played'
+// const scope = 'user-read-recently-played'
 
 export const getAccessToken = async () => {
     const response = await fetch(TOKEN_ENDPOINT, {
@@ -13,30 +13,24 @@ export const getAccessToken = async () => {
             'Content-Type': 'application/x-www-form-urlencoded', 
         },
         body: new URLSearchParams({
-        grant_type: 'refresh_token',
-        refresh_token,
+            grant_type: 'refresh_token',
+            refresh_token,
         }),
     })
 
     return response.json()
 }
-const LAST_PLAYED_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`;
+const LAST_PLAYED_ENDPOINT = `https://api.spotify.com/v1/me/player/recently-played`;
 
 
-export const getLastPlayedTracks = async () => {
+export const getLastPlayedTracks = async (limit ) => {
+    let songLimit = limit ? limit : 10;
     const { access_token } = await getAccessToken()    
-    return fetch(LAST_PLAYED_ENDPOINT, {
+    return fetch(`${LAST_PLAYED_ENDPOINT}?limit=${songLimit}`, {
         headers: {
             Authorization: `Bearer ${access_token}`
-        }    
+        },
+
     })
 }
 
-// export const getTracks =async () => {
-//     const response = await getLastPlayedTracks();
-//     const { items } = await response.json()
-//     return items
-// }
-
-// const tracks = await getTracks();
-// console.log(tracks[0])
