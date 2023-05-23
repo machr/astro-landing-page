5f251be8b9f881239b9ffcf02590bb08346f943c
Mark Christiansen <hi@powellmark.com>
2023-05-23 16:09:31 +1000

    Create React component, move logic

--- src/components/SpotifyCard.jsx
+++ src/components/SpotifyCard.jsx
@@ -1,19 +1,10 @@
-import { getLastPlayedTracks } from '../lib/spotify'
-
-const response = await getLastPlayedTracks(10)
-const { items } = await response.json();
-const formattedTracks = items.map(({track}) => ({
-    artist: track.artists.map((_artist) => _artist.name).join(', '),
-    title: track.name,
-    trackUrl: track.external_urls.spotify
-}))
-
-export default function SpotifyCard() {
+export default function SpotifyCard({songs}) {
+    console.log(songs)
     return (
     <section>
         <h3>Last 10 Played Songs</h3>
         <ol>
-            {formattedTracks.map(track => (
+            {songs.map(track => (
                 <li><a href={track.trackUrl}>{track.artist} – {track.title}</a></li>
                 ))}
         </ol>
