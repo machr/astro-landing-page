e4bb8d6797c98e504ae53003040dc9af6ce25618
Mark Christiansen <hi@powellmark.com>
2023-05-23 13:58:09 +1000

    Refactor

--- netlify/functions/spotify/index.js
+++ netlify/functions/spotify/index.js
@@ -9,45 +9,49 @@ const LAST_PLAYED_ENDPOINT = `https://api.spotify.com/v1/me/player/recently-play
 const handler = async () =>  {
     const { access_token } = await getAccessToken()
     const response = await getLastPlayedTracks(access_token)
+    console.log(response.data)
     return {
       statusCode: 200,
       body: JSON.stringify(response.data)
     };
   };
 
-
-export const getAccessToken = async () => {
+  export const getAccessToken = async () => {
     try {
-        const response = await fetch(TOKEN_ENDPOINT, {
-            method: 'POST',
-            headers: {
-                Authorization: `Basic ${encoded}`,
-                'Content-Type': 'application/x-www-form-urlencoded', 
-            },
-            body: new URLSearchParams({
-                grant_type: 'refresh_token',
-                refresh_token,
-            }),
-        })
-    
-        return response.json()
+      const response = await fetch(TOKEN_ENDPOINT, {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${encoded}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          grant_type: 'refresh_token',
+          refresh_token,
+        }),
+      });
+  
+      return response.json();
     } catch (err) {
-        console.error(err)
+      console.error(err);
+      throw err; // Throw the error to be caught by the calling function
     }
-}
+  };
 
 
 
-export const getLastPlayedTracks = async (token, limit ) => {
+  export const getLastPlayedTracks = async (token, limit) => {
     let songLimit = limit ? limit : 10;
     const response = await fetch(`${LAST_PLAYED_ENDPOINT}?limit=${songLimit}`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        },
-
-    })
-    return response.json()
-}
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  
+    const data = await response.json();
+    console.log(data); // Log the entire response
+  
+    return data;
+  };
 
 
 
