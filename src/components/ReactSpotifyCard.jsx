01d42f2baaf9b42c3b00345f4a36d2b9fe694037
Mark Christiansen <hi@powellmark.com>
2023-06-02 20:33:49 +1000

    refactor

--- src/components/ReactSpotifyCard.jsx
+++ src/components/ReactSpotifyCard.jsx
@@ -1,26 +1,35 @@
-import react from '@astrojs/react'
-// import { getLastPlayedTracks } from '../lib/spotify'
-// const response = await getLastPlayedTracks(10)
+import React, { useEffect, useState } from 'react';
 
 const getSpotifySongs = async () => {
-    const response = fetch('/api/spotify')
-    console.log('getSPotifySongs response', response)
-    const { items } = response;
-    return items
-}
+  const response = await fetch('https://machr-landing-page.netlify.app/api/spotify/api/spotify');
+  const data = await response.json();
+  return data.items;
+};
 
-const songs = await getSpotifySongs()
+const ReactSpotifyCard = () => {
+  const [formattedTracks, setFormattedTracks] = useState([]);
 
-const formattedTracks = songs.map(({track}) => ({
-    artist: track.artists.map((_artist) => _artist.name).join(', '),
-    title: track.name,
-    trackUrl: track.external_urls.spotify
-}))
+  useEffect(() => {
+    const fetchSpotifySongs = async () => {
+      const songs = await getSpotifySongs();
+      const tracks = songs.map(({ track }) => ({
+        artist: track.artists.map((_artist) => _artist.name).join(', '),
+        title: track.name,
+        trackUrl: track.external_urls.spotify,
+      }));
+      setFormattedTracks(tracks);
+    };
 
-export default function SpotifyCard() {
-    return (
-        <>
-        <div>Spotify Card</div>
-        </>
-    )
-}
+    fetchSpotifySongs();
+  }, []);
+
+  console.log(formattedTracks);
+
+  return (
+    <>
+      <div>Spotify Card</div>
+    </>
+  );
+};
+
+export { ReactSpotifyCard };
